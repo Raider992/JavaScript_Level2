54c1240512e61ceb7f8ea834f8e86c590690572e
Raider992 <Raider992@users.noreply.github.com>
2019-04-28 00:44:15 +0300

    123

--- script.js
+++ script.js
@@ -1,19 +1,83 @@
 "use strict";
-const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
-];
 
-const goodList = {
 
-    renderGoodsItem(title = 'Nothing', price = 0) {
-        return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-    },
-
-    renderGoodsList(list) {
-        document.querySelector('.goods-list').innerHTML = list.map(item => this.renderGoodsItem(item.title, item.price)).join('');
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
     }
-};
-goodList.renderGoodsList(goods);
+
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
+
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+
+    fetchGoods() {
+        this.goods = [
+            {title: 'Shirt', price: 150},
+            {title: 'Socks', price: 50},
+            {title: 'Jacket', price: 350},
+            {title: 'Shoes', price: 250},
+        ];
+    }
+
+    countTotalPrice() {
+        let price = 0;
+        this.goods.forEach(good => {
+            price += good.price;
+        });
+        console.log(price);//чтоб показать, что работает
+        return price;
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+}
+
+class Basket {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(){}           //мы будем добавлять элементы в корзину
+
+    removeItem(){}        //и удалять их из неё, если не подошли
+
+    countTotalPrice(){}   //подсчёт общей стоимости товаров
+
+    countTotalNumber() {} //подсчитать общее число предметов в корзине
+
+    submitAndBuy(){}      //не уверен, будем ли мы это реализовать в курсе, но должна же быть у корзины кнопка "оформить заказ"
+
+    render() {}           //это всё, конечно, нужно отрисовывать
+}
+
+class basketItem {
+    constructor(title, price, quantity) {
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><p>${this.quantity}</p></div>`;
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.countTotalPrice();
